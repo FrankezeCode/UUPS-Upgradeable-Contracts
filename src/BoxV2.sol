f165c4fd38b90177492850b334b191b743097833
FrankezeCode <franklynezeugonna@gmail.com>
2024-08-14 20:51:23 -0700

    initial commit

---
+++ src/BoxV2.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is  Initializable , UUPSUpgradeable  , OwnableUpgradeable {
+    uint256 internal number;
+
+    //We cant use a constructor in a proxy
+    // constructor() {
+    //     _disableInitializers();
+    // }
+    
+    //This function now acts as our constructor
+    function initialize() public initializer {
+        __Ownable_init(); // sets owner to msg.sender
+        __UUPSUpgradeable_init();
+    }
+
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
